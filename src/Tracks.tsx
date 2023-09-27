ea6974ab6de457532aab73ba02f4669423c198a7
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-09-27 21:31:38 +0800

    prices updates

--- src/Tracks.tsx
+++ src/Tracks.tsx
@@ -1,7 +1,7 @@
 export default [
     {
       id: 1,
-      price: "$1,500",
+      price: "$1,200",
       name: "Circuit de Monaco",
       description: "Opened in 1929, Circuit de Monaco has always been an unlikely and impractical venue for top-flight motor racing. Yet the race in the tiny principality on the French Riviera remains one of Formula 1’s blue ribbon events, even though wheel-to-wheel racing is a little trickier given the increase in F1 car size over the years. Despite its limitations, Circuit de Monaco remains hugely challenging for drivers and an incredible spectacle for those fortunate enough to attend. Why go? Glamour, super yachts, people-watching and the chance to see modern F1 cars on the ragged edge at very close quarters." ,
       image:
@@ -9,7 +9,7 @@ export default [
     },
     {
         id: 2,
-        price: "$1,500",
+        price: "$1,550",
         name: "Silverstone",
         description: "Built on the site of a World War II airstrip, Silverstone was opened in 1948 and held the first race of the modern Formula 1 World Championship two years later with King George VI in attendance. Despite being rather flat and featureless, the fast and flowing circuit in the English countryside has delivered many of Formula 1’s most exciting races over the years. Firmly established as the sole host of the British Grand Prix since the late 1980s, Silverstone remains a supreme test of drivers’ skills and is also one of the best-attended races on the entire calendar. Why go? Passionate and knowledgeable local fans, friendly atmosphere and an action-packed event schedule, on and off the track." ,
         image:
@@ -17,7 +17,7 @@ export default [
     },
     {
       id: 3,
-      price: "$1,500",
+      price: "$2,540",
       name: "Suzuka Circuit",
         description: "Opened in 1960 as a test track for Honda, Suzuka has been the home of the Japanese Grand Prix since 1987. A favourite among drivers and fans alike, Suzuka’s unique ‘figure of eight’ layout has produced some of the most exciting F1 races of the past 35 years. As one of the final races on the calendar for much of its existence, the Japanese Grand Prix has also been the scene of multiple championship deciders, including infamous accidents between title contenders Alain Prost and Ayrton Senna in 1989 and 1990. Why go? Excellent trackside views, passionate local fans and the chance to experience Japan’s unique history and culture." ,
         image:
@@ -25,7 +25,7 @@ export default [
     },
     {
       id: 4,
-      price: "$1,500",
+      price: "$1,800",
       name: "Hungaroring",
         description: "The first and only race held behind the Iron Curtain made its debut on the 1986 Formula 1 calendar and has been held every year since. Only Monza can claim a longer continuous presence on the F1 calendar than the Hungaroring! The tight and twisty circuit on the outskirts of Budapest has delivered more than its fair share of exciting races over the years, and the Hungarian Grand Prix remains a popular summer fixture on the European F1 calendar. Why go? Warm weather, excellent spectator views and close proximity to the affordable and vibrant city of Budapest." ,
         image:

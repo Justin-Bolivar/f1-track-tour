24e433b5dba6370871533cf96e95f456070d2804
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-10-01 09:06:14 +0800

    Read More button now Works

--- src/List.tsx
+++ src/List.tsx
@@ -1,17 +1,24 @@
+import { useState } from "react";
 
 const List = ({circuit}) => {
+  const [readMore, setReadMore] = useState(false);
   return (
     <>
       {circuit.map((track: {id: number, price: string, name: string, description: string, image: string}) => {
         const { id, price, name, description, image } = track;
         return (
           <article key={id} className="single-tour">
-            <img src={image} alt={name} />
+            <img src={image} alt={name} className="img" />
             <span className="tour-price">{price}</span>
             <div className="tour-info">
-              <h5>{name}</h5>
-              <p>{description} <button className="info-btn">read more</button> </p>
-              <button className="delete-btn btn-block btn">Not Interested</button>
+                <h5>{name}</h5>
+                <p>
+                  {readMore ? description: description.substring(0, 100) + "..."}
+                   <button onClick={() => setReadMore(!readMore)} className="info-btn">
+                      {readMore ? 'Read Less' : 'Read More'}
+                   </button> 
+                </p>
+                <button className="delete-btn btn-block btn">Not Interested</button>
             </div>
           </article>
         );
